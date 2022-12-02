3b911a59c3a34019500da5f4e4d9b6ebbdcfe919
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-02 11:50:17 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,40 @@
+export const item = {
+    "id": "62973b3c-653a-4f3e-85d3-4a6b0cfae859",
+    "searches": [
+        {
+            "id": "f9ec62e7-f08a-4814-a1de-722576b9884d",
+            "timer": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png",
+            "older_search": "subject",
+            "older_search_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png",
+            "top_left_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+        },
+        {
+            "id": "9c599961-c0d0-4567-b85d-2556801cd9c5",
+            "timer": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/256.png",
+            "older_search": "relate",
+            "older_search_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png",
+            "top_left_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/151.png"
+        },
+        {
+            "id": "2e0f04a4-cfd9-4ef1-8462-c8f395ed28f0",
+            "timer": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png",
+            "older_search": "appear",
+            "older_search_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png",
+            "top_left_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+        },
+        {
+            "id": "ca2750c7-f8fa-4631-bd1e-c3b08a9db3df",
+            "timer": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png",
+            "older_search": "play",
+            "older_search_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png",
+            "top_left_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/12.png"
+        },
+        {
+            "id": "cda718fd-9334-40c2-b5e5-ced10efc0bcd",
+            "timer": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/106.png",
+            "older_search": "enjoy",
+            "older_search_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png",
+            "top_left_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+        }
+    ]
+}
